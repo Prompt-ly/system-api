ae6bfc4b8eb3c991fdbb2ab3bfeeea3f5441ecbf
Brandon Dodd <brandonharleydodd@gmail.com>
2025-10-22 12:14:33 +0100

    Include Setting type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export type { App } from "./modules/apps";
 export type { Process } from "./modules/process";
+export type { Setting } from "./modules/settings";
 export * as Windows from "./windows/windows";
